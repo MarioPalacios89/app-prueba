45919572da7af60b16a505a156b9c23259586f74
Mario <desarrollador89_usi@minedu.gob.pe>
2021-05-03 19:35:44 -0500

    WIP: se han agregado mas componentes

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutModule } from '@angular/cdk/layout';
 import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AngularMaterialModule } from '../core/angular-material/angular-material.module';
 import { FeaturesRoutingModule } from './features-routing.module';
+
 
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { MainComponent } from './main/main.component';
@@ -12,6 +14,7 @@ import { HeaderComponent } from './header/header.component';
   declarations: [SidebarComponent, MainComponent, HeaderComponent],
   imports: [
     CommonModule,
+    FlexLayoutModule,
     FeaturesRoutingModule,
     LayoutModule,
     AngularMaterialModule,
